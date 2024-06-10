82592a497425fa94968cd17839f8acadb5f9fd0e
Mehwish9474 <132438858+Mehwish9474@users.noreply.github.com>
2024-06-10 13:33:09 -0500

    Updated Homework 1

--- Assignments/Assignment 01/DynamicArray.java
+++ Assignments/Assignment 01/DynamicArray.java
@@ -1,7 +1,19 @@
-/* 
+/*
+ * In the class below, write two methods:
+ * 
+ * First, method resize() to expand the existing array data by doubling its size.
+ *
+ * Second, method contains(String string) that returns true if String string already
+ * exists in array data and false otherwise.
+ *
+ * Your code must have comments explaining what is done and why it is done in the
+ * way you do it.
+ *
+ * DO NOT USE any tools that require to be imported, ie, do not use the import command.
+ *
  * To save this assignment, make sure you commit your changes to your GitHub repository,
  * following the instructions in Sakai. IF YOU DO NOT COMMIT THE CHANGES, IT IS POSSIBLE THAT
- * YOUR WORK MAY BE LOST AND YOU MAY HAVE TO START ALL OVER AGAIN. Mehwish Tabassum
+ * YOUR WORK MAY BE LOST AND YOU MAY HAVE TO START ALL OVER AGAIN.
  */
 public class DynamicArray {
 
@@ -44,18 +56,37 @@ public class DynamicArray {
     } // method add
 
     /**
-     * Resizes the underlying array to accommodate more elements.
+     * Increases the size of the underlying array by doubling its size.
+     * A larger temporary array is created, contents of this.data are
+     * copied over, and the temp then replaces this.data.
      */
     private void resize() {
-        String[] newData = new String[this.data.length * 2];
-        System.arraycopy(this.data, 0, newData, 0, this.data.length);
-        this.data = newData;
+        int newSize = this.data.length * 2;
+        String[] temp = new String[newSize];
+        for (int i = 0; i < this.data.length; i++) {
+            temp[i] = this.data[i];
+        }
+        this.data = temp;
     } // method resize
 
     /**
-     * Counts the number of times the specified string appears in the array.
-     * @param string the string to count in the array
-     * @return the number of occurrences of the string
+     * Determines if a String value is present in the underlying array
+     * @param string String to search for
+     * @return true if string found in array; false otherwise.
+     */
+    public boolean contains(String string) {
+        for (int i = 0; i < this.position; i++) {
+            if (this.data[i].equals(string)) {
+                return true;
+            }
+        }
+        return false;
+    } // method contains
+
+    /**
+     * Returns the number of times a string appears in the underlying array
+     * @param string String to search for
+     * @return count of occurrences.
      */
     public int countOf(String string) {
         int count = 0;
@@ -68,19 +99,17 @@ public class DynamicArray {
     } // method countOf
 
     /**
-     * The main method to test the DynamicArray class.
-     * @param args the command line arguments
+     * Adds a string to the underlying array only if the string is not
+     * already present.
+     * @param string String to add
+     * @return true if addition was successful, false otherwise (indicating a duplicate)
      */
-    public static void main(String[] args) {
-        DynamicArray dynamicArray = new DynamicArray();
-        dynamicArray.add("dog");
-        dynamicArray.add("cat");
-        dynamicArray.add("dot");
-        dynamicArray.add("cat");
-        dynamicArray.add("bird");
+    public boolean addUnique(String string) {
+        if (!this.contains(string)) {
+            this.add(string);
+            return true;
+        }
+        return false;
+    } // method addUnique
 
-        System.out.println("Count of 'dot': " + dynamicArray.countOf("dot")); // should print 1
-        System.out.println("Count of 'cat': " + dynamicArray.countOf("cat")); // should print 2
-        System.out.println("Count of 'frog': " + dynamicArray.countOf("frog")); // should print 0
-    } // method main
 } // class DynamicArray
