22e01bc9d7ba099072bbfbb52172df9e665ff67f
Mehwish9474 <132438858+Mehwish9474@users.noreply.github.com>
2024-07-30 23:23:24 +0000

    Updated assignment 9

--- Assignments/Assignment_09/Hash271.java
+++ Assignments/Assignment_09/Hash271.java
@@ -3,49 +3,74 @@ public class Hash271 {
     /** Default size for foundation array */
     private static final int DEFAULT_SIZE = 4;
 
+    /** Default threshold for rehashing */
+    private static final double DEFAULT_THRESHOLD = 0.75;
+
     /** Foundation array of node objects */
     Node[] foundation;
+
+    /** Current load factor */
+    private double loadFactor;
+
+    /** User-specified threshold for rehashing */
+    private double threshold;
+
+    /** Number of nodes in the hash table */
+    private int nodeCount;
 
     /** Basic constructor */
     public Hash271(int size) {
         this.foundation = new Node[size];
-    } // basic constructor
+        this.threshold = DEFAULT_THRESHOLD;
+        this.loadFactor = 0.0;
+        this.nodeCount = 0;
+    } // constructor
 
     /** Default constructor */
     public Hash271() {
         this(DEFAULT_SIZE);
     } // default constructor
 
-    /**
-     * Map an integer number to one of the positions of the underlying array. This
-     * will come handy we need to find the place to chain a node.
-     * 
-     * @param value int to map to one of the array positions
-     * @return int with the integer division remainder between the input value and
-     *         the length of the array
-     */
-    private int computeArrayPosition(int value) {
-        return value % this.foundation.length;
+    /** Method to compute array position */
+    private int computeArrayPosition(int hashCode) {
+        return Math.abs(hashCode % this.foundation.length);
     } // method computeArrayPosition
 
-    /**
-     * Chain a node to the underlying array
-     * 
-     * @param node Node to chain to the underlying array
-     */
+    /** Rehash method to resize and rehash the nodes */
+    private void rehash() {
+        Node[] oldFoundation = this.foundation;
+        this.foundation = new Node[oldFoundation.length * 2];
+        this.nodeCount = 0;
+
+        // Move nodes from the old array to the new array
+        for (Node node : oldFoundation) {
+            while (node != null) {
+                Node next = node.getNext();
+                node.setNext(null);
+                this.put(node);
+                node = next;
+            }
+        }
+    } // method rehash
+
+    /** Method to add a node to the hash table */
     public void put(Node node) {
-        // Operate only is node is not null
         if (node != null) {
-            // Use the node's hashcode to determine is position in
-            // the underlying array
+            // Use the hash code of the node to determine its position in the underlying array
             int destination = computeArrayPosition(node.hashCode());
-            // If the position in the array is occupied by another node,
-            // place that node under the new node we wish to insert
+            // If the position in the array is occupied by another node, place that node under the new node we wish to insert
             if (this.foundation[destination] != null) {
                 node.setNext(this.foundation[destination]);
             }
             // Put the new node to the array position
             this.foundation[destination] = node;
+            this.nodeCount++;
+            this.loadFactor = (double) this.nodeCount / this.foundation.length;
+
+            // Check if rehashing is needed
+            if (this.loadFactor > this.threshold) {
+                rehash();
+            }
         }
     } // method put
 
@@ -91,5 +116,5 @@ public class Hash271 {
         h.put(new Node("C"));
         h.put(new Node("C#"));
         System.out.println(h);
-    }
-}
+    } // method main
+} // class Hash271
